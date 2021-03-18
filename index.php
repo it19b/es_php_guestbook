ccb98ab831044c702d51aa9c70a8aec8a900b46f
48969167 <mail.eduard.schwarzkopf@gmail.com>
2021-03-18 11:47:50 +0100

    glaube ist fertig

--- index.php
+++ index.php
@@ -1,66 +1,29 @@
+<?php
 
+include 'SQL.php';
+
+$result = null;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $sql = new SQL();
+  $result = $sql->InsertEntry($_POST);
+}
+
+?>
 <!DOCTYPE HTML>  
 <html>
 <head>
 </head>
 <body>
 
-<?php
 
-$servername = "localhost";
-$username = "admin";
-$password = "admin";
-$database = "versicherungen";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $database);
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully";
-
-
-// define variables and set to empty values
-$name = $email = $gender = $comment = $website = "";
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = test_input($_POST["name"]);
-  $email = test_input($_POST["email"]);
-  $website = test_input($_POST["website"]);
-  $comment = test_input($_POST["comment"]);
-  $gender = test_input($_POST["gender"]);
-
-  $sql = "INSERT INTO guestbook (name, email, website, comment, gender) VALUES ('$name', '$email', '$website', '$comment', '$gender')";
-  echo $sql;
-
-
-    if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-    } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-
-    $conn->close();
-
-}
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-?>
-
-<h2>PHP Form Validation Example</h2>
+<h2>Banana</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">  
   Name: <input type="text" name="name">
   <br><br>
-  E-mail: <input type="email" placeholder="Email Adresse" name="email" required>
+  E-mail: <input type="email" placeholder="Email Adresse" name="email">
   <br><br>
-  Website: <input type="url" name="website" required>
+  Website: <input type="url" name="website">
   <br><br>
   Comment: <textarea name="comment" maxlength="600" rows="5" cols="40"></textarea>
   <br><br>
@@ -73,17 +36,15 @@ function test_input($data) {
 </form>
 
 <?php
-echo $_SERVER["REQUEST_METHOD"];
-echo "<h2>Your Input:</h2>";
-echo $name;
-echo "<br>";
-echo $email;
-echo "<br>";
-echo $website;
-echo "<br>";
-echo $comment;
-echo "<br>";
-echo $gender;
+
+if ($result) {
+  // redirect to all posts
+  header('Location: ' . "/es_php_guestbook/posts.php");
+  die();
+} elseif (is_string($result)) {
+  ?> <h3> <?php $result ?> </h3> <?php
+} 
+
 ?>
 
 </body>
---
+++ posts.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'SQL.php';
+$sql = new SQL();
+$result = $sql->GetGuesbookPosts();
+?>
+
+<htlm>
+<body>
+<h2>Posts</h2>
+<?php 
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    $name = $row["name"];
+    $gender = $row["gender"];
+    $email = $row["email"];
+    $website = $row["website"];
+    $comment = $row["comment"];
+
+    $html = "
+        <h3>$name ($gender)</h3>
+        <h5>Email: $email | Webseite: $website</h5>
+
+        <div>$comment</div>
+        <hr>
+    ";
+
+    echo $html;
+    
+  }
+} else {
+  echo "0 posts";
+}
+
+?>
+</body>
+</html>
